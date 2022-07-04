1bfe604a09bf3176b16a3027fb3e4954aa0954c3
FacundoBaez1994 <fbaez@fi.uba.ar>
2022-07-04 16:29:47 -0300

    Fixeo de bug seleccion de jugadores

--- src/main/java/edu/fiuba/algo3/controlador/sonido/HandlerBuenoVamoAJuga.java
+++ src/main/java/edu/fiuba/algo3/controlador/sonido/HandlerBuenoVamoAJuga.java
@@ -3,7 +3,7 @@ package edu.fiuba.algo3.controlador.sonido;
 import java.io.File;
 
 public class HandlerBuenoVamoAJuga {
-
+//inicio de juego audio
     private static HandlerSonido sonido =
             new HandlerSonido(new File("src/recursos/sonidos/vamoajugar.mp3"));
     static public void reproducirSonido(){
